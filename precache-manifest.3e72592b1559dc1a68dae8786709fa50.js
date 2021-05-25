46359d9550d10f5099fa6edef8138133bae26572
Rafael <rkyriakou@uth.gr>
2021-05-25 21:55:29 +0300

    Updates

---
+++ precache-manifest.3e72592b1559dc1a68dae8786709fa50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b94f27fc40569c20a3ef45a0f1dda8",
+    "url": "/Auction-app/index.html"
+  },
+  {
+    "revision": "391a7e38b11f0a7332b5",
+    "url": "/Auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "afc0f36e78959a8f4fe2",
+    "url": "/Auction-app/static/js/2.a2812b0c.chunk.js"
+  },
+  {
+    "revision": "391a7e38b11f0a7332b5",
+    "url": "/Auction-app/static/js/main.1138c64f.chunk.js"
+  },
+  {
+    "revision": "f7472452af8151a2b3cc",
+    "url": "/Auction-app/static/js/runtime-main.0409cdc2.js"
+  }
+]);
